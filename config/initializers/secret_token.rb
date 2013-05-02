14ad770290c02793ae01dfdc1fdbf11fbfc66390
ANEESH MENON <presoft@smtp.gmail.com>
2013-05-02 16:17:39 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DataManipulation::Application.config.secret_token = '357ce1b180b0d2d1243e8d78f7dfc74cb261435e1a1807daaf1d205f234cd979738371f1df83ac6d434a13a08c2c22235ae44b2ad157b3a6501d7ae77625f891'
