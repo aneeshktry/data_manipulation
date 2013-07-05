c024c6a5679a2b5beeee69aaaef4028752543f63
ANEESH MENON <presoft@smtp.gmail.com>
2013-07-05 15:14:28 +0530

    email extraction

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,29 +1,26 @@
 class HomeController < ApplicationController
+  require 'csv'
   def home
     
   end
 
   def extract
-    puts "@"*100
-    puts "#{params[:text]}"
-    puts "@"*100
-    require 'csv'
-    source_file_path = ARGV[0]
-    destination_path = ARGV[1]
+    #    puts "@"*100
+    file_path = "#{Rails.root}/tmp/emails/emails.csv"
+    text = params[:text]
+    #    puts "@"*100
     data_list = []
     data_list << ["email"]
-    file = File.open(source_file_path,'r:ascii-8bit')
-    if file
-      file.each do |line|
-        line_list = line.split(" ")
-        line_list.each do |str|
-          if str.strip.match(/^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/)
-            data_list << [str]
-          end
-        end
+    line_list = text.split(" ")
+    line_list.each do |str|
+      if str.strip.match(/^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/)
+        data_list << [str]
       end
-      insert_into_csv(data_list,destination_path)
     end
+    insert_into_csv(data_list,file_path)
+#    sleep(10)
+    send_file file_path, :type=>'text/csv' if File.exist?(file_path)
+#    File.delete(file_path) if File.exist?(file_path)
   end
 
   def insert_into_csv(data_list,file_path)
@@ -37,3 +34,19 @@ class HomeController < ApplicationController
     end
   end
 end
+#source_file_path = ARGV[0]
+#    destination_path = ARGV[1]
+#    data_list = []
+#    data_list << ["email"]
+#    file = File.open(source_file_path,'r:ascii-8bit')
+#    if file
+#      file.each do |line|
+#        line_list = line.split(" ")
+#        line_list.each do |str|
+#          if str.strip.match(/^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/)
+#            data_list << [str]
+#          end
+#        end
+#      end
+#      insert_into_csv(data_list,destination_path)
+#    end
