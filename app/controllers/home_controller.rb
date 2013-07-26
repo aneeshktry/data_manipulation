862846dfa0a2de67abe6676b71d37aca43c0d462
ANEESH MENON <presoft@smtp.gmail.com>
2013-07-26 17:48:45 +0530

    implimenting clear button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
   require 'csv'
   def home
-    @text_file = Assets.new
+    @text_file = TextAssets.new
   end
 
   def text_extract
     #    puts "@"*100
-    file_path = "#{Rails.root}/tmp/emails/emails.csv"
+    file_path = "#{Rails.root}/tmp/emails/emails#{Time.now.strftime("%Y%m%d%H%M%S")}.csv"
     text = params[:text]
     #    puts "@"*100
     data_list = []
@@ -20,11 +20,11 @@ class HomeController < ApplicationController
     insert_into_csv(data_list,file_path)
 #    sleep(10)
     send_file file_path, :type=>'text/csv' if File.exist?(file_path)
-    File.delete(file_path) if File.exist?(file_path)
+#    File.delete(file_path) if File.exist?(file_path)
   end
 
   def file_extract
-    @text_file = Assets.new(:document=>params[:text][:document])
+    @text_file = TextAssets.new(:document=>params[:text][:document])
     @text_file.valid?
     puts "#{@text_file.errors.full_messages}".red
     @text_file.save
@@ -44,10 +44,10 @@ class HomeController < ApplicationController
         end
       end
       insert_into_csv(data_list,destination_path)
-      @text_file.destroy
+#      @text_file.destroy
     end
     send_file destination_path, :type=>'text/csv' if File.exist?(destination_path)
-    File.delete(destination_path) if File.exist?(destination_path)
+#    File.delete(destination_path) if File.exist?(destination_path)
   end
 
   def insert_into_csv(data_list,file_path)
@@ -61,19 +61,3 @@ class HomeController < ApplicationController
     end
   end
 end
-#source_file_path = ARGV[0]
-#    destination_path = ARGV[1]
-#    data_list = []
-#    data_list << ["email"]
-#    file = File.open(source_file_path,'r:ascii-8bit')
-#    if file
-#      file.each do |line|
-#        line_list = line.split(" ")
-#        line_list.each do |str|
-#          if str.strip.match(/^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$/)
-#            data_list << [str]
-#          end
-#        end
-#      end
-#      insert_into_csv(data_list,destination_path)
-#    end
